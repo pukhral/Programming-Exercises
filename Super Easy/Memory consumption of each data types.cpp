93000a1ba93fb17aa475f8b9704c3364caa57991
Mayank Pukhral <mayankpukhral05@gmail.com>
2022-06-22 00:38:00 +0530

    Upload second program

---
+++ Super Easy/Memory consumption of each data types.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout << "Integer takes " << sizeof(int) << " bytes." << endl;
+    cout << "Float takes " << sizeof(float) << " bytes." << endl;
+    cout << "Double takes " << sizeof(double) << " bytes" << endl;
+    cout << "Character takes " << sizeof(char) << " bytes" << endl;
+    cout << "Boolean takes " << sizeof(bool) << " bytes" << endl;
+
+    return 0;
+}
