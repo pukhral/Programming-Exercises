4e7e6f849513df81a2b582750cf01f488ea26f73
Mayank Pukhral <mayankpukhral05@gmail.com>
2022-06-23 23:33:51 +0530

    program to find next character'

---
+++ Super Easy/Next Character.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char m = 'M', a1 = 'A', y = 'Y', a2 = 'A', n = 'N', k = 'K';
+    m = m + 1;
+    a1 = a1 + 1;
+    y = y + 1;
+    a2 = a2 + 1;
+    n = n + 1;
+    k = k + 1;
+    cout << "Next letter after M is " << m << endl;
+    cout << "Next letter after A is " << a1 << endl;
+    cout << "Next letter after Y is " << y << endl;
+    cout << "Next letter after A is " << a2 << endl;
+    cout << "Next letter after N is " << m << endl;
+    cout << "Next letter after K is " << k << endl;
+
+    return 0;
+}
