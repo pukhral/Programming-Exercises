452539f94e14c11bbd57436b6c606cb8d2a49091
Mayank Pukhral <mayankpukhral05@gmail.com>
2022-06-23 23:23:29 +0530

    Upload an exercise

---
+++ Super Easy/ASCII Code of characters.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout << "ASCII Code of M is " << 'M' + 0 << endl;
+    cout << "ASCII Code of A is " << 'A' + 0 << endl;
+    cout << "ASCII Code of Y is " << 'Y' + 0 << endl;
+    cout << "ASCII Code of A is " << 'A' + 0 << endl;
+    cout << "ASCII Code of N is " << 'N' + 0 << endl;
+    cout << "ASCII Code of K is " << 'K' + 0 << endl;
+
+    return 0;
+}
