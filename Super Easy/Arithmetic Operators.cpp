df88df38979d543775afcd2757bae8cf2d546385
Mayank Pukhral <mayankpukhral05@gmail.com>
2022-06-25 23:50:19 +0530

    Write a progran to calculate all the mathematical operations in c++

---
+++ Super Easy/Arithmetic Operators.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int operand1, operand2;
+    cout << "Enter the first number :- ";
+    cin >> operand1;
+    cout << "Enter the second number :- ";
+    cin >> operand2;
+    cout << "Addition = " << operand1 + operand2 << endl;
+    cout << "Subtraction = " << operand1 - operand2 << endl;
+    cout << "Multiplication = " << operand1 * operand2 << endl;
+    cout << "Division = " << operand1 / operand2 << endl;
+    cout << "Modulus = " << operand1 % operand2 << endl;
+}
